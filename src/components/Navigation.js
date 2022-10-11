75c11241a990b7fba173408172079da37c93caf5
Jonathan Kuhn <jkuhn96@gmail.com>
2022-10-10 20:24:21 -0400

    Added initial components and styling

---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Navigation.module.css'
+
+const Navigation = () => {
+  return (
+    <div className={styles.navigation}>Navigation</div>
+  )
+}
+
+export default Navigation
---
+++ src/components/SubContents.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SubContent.module.css'
+
+const SubContents = () => {
+  return (
+    <div className={styles.subContent}>SubContents</div>
+  )
+}
+
+export default SubContents
